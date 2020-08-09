6d98d8f02699c88501946b76ebb2bc915cf5acc1
Maurice Kevenaar <maurice@kevenaar.name>
2020-08-09 13:08:26 +0200

    (GH-13) Added tests

--- build/Test.ps1
+++ build/Test.ps1
@@ -3,7 +3,8 @@ Write-Host "Current working directory: $pwd"
 
 $resultsFile = '.\TestsResults.xml'
 $testFiles   = Get-ChildItem "$pwd\tests" -Recurse | Where-Object {$_.FullName -match 'Tests.ps1$'} | Select-Object -ExpandProperty FullName
-$results     = Invoke-Pester -Script $testFiles -OutputFormat NUnitXml -OutputFile $resultsFile -PassThru
+$sourceFiles = Get-ChildItem "$pwd\chocolatey-diff" -Recurse | Where-Object {$_.FullName -match 'ps1$'} | Select-Object -ExpandProperty FullName
+$results     = Invoke-Pester -Script $testFiles -CodeCoverage $sourceFiles -OutputFormat NUnitXml -OutputFile $resultsFile -PassThru
 
 if (($results.FailedCount -gt 0) -or ($results.PassedCount -eq 0) -or ($null -eq $results)) {
   throw "$($results.FailedCount) tests failed."
