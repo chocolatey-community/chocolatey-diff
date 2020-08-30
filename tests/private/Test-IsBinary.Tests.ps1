d1b93a4db0fa82e566d72063b1fd7290cb23da23
Maurice Kevenaar <maurice@kevenaar.name>
2020-08-30 18:44:57 +0200

    (GH-13) Switched Windows detection

--- tests/private/Test-IsBinary.Tests.ps1
+++ tests/private/Test-IsBinary.Tests.ps1
@@ -11,8 +11,8 @@ Describe "Test-IsBinary tests" {
     InModuleScope chocolatey-diff {
         Context 'Test-IsBinary Tests' {
             $testCases = @(
-                @{ Expected = $false; File = If (Test-Path (Join-Path -Path $env:windir -ChildPath "win.ini") -PathType Leaf) { Join-Path -Path $env:windir -ChildPath "win.ini" } else { '/etc/hosts' }; }
-                @{ Expected = $true; File = If (Test-Path (Join-Path -Path $env:windir -ChildPath "notepad.exe") -PathType Leaf) { Join-Path -Path $env:windir -ChildPath "notepad.exe" } else { '/bin/cat' }; }
+                @{ Expected = $false; File = If (![string]::IsNullOrEmpty($env:windir)) { Join-Path -Path $env:windir -ChildPath "win.ini" } else { '/etc/hosts' }; }
+                @{ Expected = $true; File = If (![string]::IsNullOrEmpty($env:windir)) { Join-Path -Path $env:windir -ChildPath "notepad.exe" } else { '/bin/cat' }; }
             )
             It "Test-IsBinary should return <Expected> on file <File>" -TestCases $testCases {
                 Param($File, $Expected)
