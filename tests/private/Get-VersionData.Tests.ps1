a0fbffb4e3785d40bb19eb7d5685f01b3f5c0cb1
Maurice Kevenaar <maurice@kevenaar.name>
2020-09-03 14:20:48 +0200

    (GH-13) Move Mock to BeforeAll

--- tests/private/Get-VersionData.Tests.ps1
+++ tests/private/Get-VersionData.Tests.ps1
@@ -4,35 +4,48 @@ Describe "Get-VersionData tests" {
 
     Context 'Get-VersionData normal Tests ' {
 
+        BeforeAll {
+            Mock 'Invoke-RestMethod' { [xml] "<?xml version=`"1.0`" encoding=`"utf-8`" standalone=`"yes`"?>
+            <entry xml:base=`"http://chocolatey.org/api/v2/`" xmlns:d=`"http://schemas.microsoft.com/ado/2007/08/dataservices`" xmlns:m=`"http://schemas.microsoft.com/ado/2007/08/dataservices/metadata`" xmlns=`"http://www.w3.org/2005/Atom`">
+              <m:properties xmlns:m=`"http://schemas.microsoft.com/ado/2007/08/dataservices/metadata`" xmlns:d=`"http://schemas.microsoft.com/ado/2007/08/dataservices`">
+                <d:GalleryDetailsUrl>https://chocolatey.org/packages/${PackageName}/${PackageVersion}</d:GalleryDetailsUrl>
+                <d:Published m:type=`"Edm.DateTime`">${PublishedDate}</d:Published>
+                <d:IsApproved m:type=`"Edm.Boolean`">${IsApproved}</d:IsApproved>
+                <d:PackageStatus>${PackageStatus}</d:PackageStatus>
+                <d:PackageSubmittedStatus>${PackageSubmittedStatus}</d:PackageSubmittedStatus>
+              </m:properties>
+            </entry>" } -ModuleName 'chocolatey-diff'
+        }
+
         #region Discovery
         $testCases = @(
             @{
-                PackageName = 'MyPackage';
-                PackageVersion = '0.1.0';
-                PackageStatus = 'Submitted';
+                PackageName            = 'MyPackage';
+                PackageVersion         = '0.1.0';
+                PackageStatus          = 'Submitted';
                 PackageSubmittedStatus = 'Ready';
-                PackageExpectedStatus = 'submitted';
+                PackageExpectedStatus  = 'submitted';
             }
             @{
-                PackageName = 'MyPackage';
-                PackageVersion = '0.1.0';
-                PackageStatus = 'Submitted';
+                PackageName            = 'MyPackage';
+                PackageVersion         = '0.1.0';
+                PackageStatus          = 'Submitted';
                 PackageSubmittedStatus = 'Waiting';
-                PackageExpectedStatus = 'waiting';
+                PackageExpectedStatus  = 'waiting';
             }
             @{
-                PackageName = 'MyPackage';
-                PackageVersion = '0.1.0';
-                PackageStatus = 'Approved';
+                PackageName            = 'MyPackage';
+                PackageVersion         = '0.1.0';
+                PackageStatus          = 'Approved';
                 PackageSubmittedStatus = 'Ready';
-                PackageExpectedStatus = 'approved';
+                PackageExpectedStatus  = 'approved';
             }
             @{
-                PackageName = 'MyPackage';
-                PackageVersion = '0.1.0-beta';
-                PackageStatus = 'Exempted';
+                PackageName            = 'MyPackage';
+                PackageVersion         = '0.1.0-beta';
+                PackageStatus          = 'Exempted';
                 PackageSubmittedStatus = 'Ready';
-                PackageExpectedStatus = 'exempted';
+                PackageExpectedStatus  = 'exempted';
             }
         )
         #endregion Discovery
@@ -45,20 +58,10 @@ Describe "Get-VersionData tests" {
             if ($PackageStatus -eq "Approved" -or $PackageStatus -eq "Exempted") {
                 $PublishedDate = (Get-Date).ToUniversalTime().ToString('yyyy-MM-ddThh:mm:ss.fff')
             }
-            Mock 'Invoke-RestMethod' { [xml] "<?xml version=`"1.0`" encoding=`"utf-8`" standalone=`"yes`"?>
-            <entry xml:base=`"http://chocolatey.org/api/v2/`" xmlns:d=`"http://schemas.microsoft.com/ado/2007/08/dataservices`" xmlns:m=`"http://schemas.microsoft.com/ado/2007/08/dataservices/metadata`" xmlns=`"http://www.w3.org/2005/Atom`">
-              <m:properties xmlns:m=`"http://schemas.microsoft.com/ado/2007/08/dataservices/metadata`" xmlns:d=`"http://schemas.microsoft.com/ado/2007/08/dataservices`">
-                <d:GalleryDetailsUrl>https://chocolatey.org/packages/${PackageName}/${PackageVersion}</d:GalleryDetailsUrl>
-                <d:Published m:type=`"Edm.DateTime`">${PublishedDate}</d:Published>
-                <d:IsApproved m:type=`"Edm.Boolean`">${IsApproved}</d:IsApproved>
-                <d:PackageStatus>${PackageStatus}</d:PackageStatus>
-                <d:PackageSubmittedStatus>${PackageSubmittedStatus}</d:PackageSubmittedStatus>
-              </m:properties>
-            </entry>" } -ModuleName 'chocolatey-diff'
             $versionData = InModuleScope 'chocolatey-diff' -Parameters @{
-                PackageName = $PackageName;
+                PackageName    = $PackageName;
                 PackageVersion = $PackageVersion
-             } {
+            } {
                 Param ($PackageName, $PackageVersion)
                 Get-VersionData -packageName $PackageName -version ([SemanticVersion]::create($PackageVersion))
             }
@@ -71,25 +74,28 @@ Describe "Get-VersionData tests" {
     }
     Context 'Get-VersionData rejected Tests ' {
 
+        BeforeAll {
+            Mock 'Invoke-RestMethod' { throw "Resource not found for the segment 'Packages'." } -ModuleName 'chocolatey-diff'
+        }
+
         #region Discovery
         $testCases = @(
             @{
-                PackageName = 'MyPackage';
-                PackageVersion = '0.1.0';
-                PackageStatus = 'Rejected';
+                PackageName            = 'MyPackage';
+                PackageVersion         = '0.1.0';
+                PackageStatus          = 'Rejected';
                 PackageSubmittedStatus = 'Ready';
-                PackageExpectedStatus = 'rejected';
+                PackageExpectedStatus  = 'rejected';
             }
         )
         #endregion Discovery
 
         It 'Fetch Package VersionData for <PackageName> package v<PackageVersion> with status <PackageStatus> and Submitted status <PackageSubmittedStatus> ' -TestCases $testCases {
             Param($PackageName, $PackageVersion, $PackageStatus, $PackageSubmittedStatus, $PackageExpectedStatus)
-            Mock 'Invoke-RestMethod' { throw "Resource not found for the segment 'Packages'." } -ModuleName 'chocolatey-diff'
             $versionData = InModuleScope 'chocolatey-diff' -Parameters @{
-                PackageName = $PackageName;
+                PackageName    = $PackageName;
                 PackageVersion = $PackageVersion
-             } {
+            } {
                 Param ($PackageName, $PackageVersion)
                 Get-VersionData -packageName $PackageName -version ([SemanticVersion]::create($PackageVersion))
             }
