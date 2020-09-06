aa6d848ce3d63cf578438af981e9b4ee5194b949
Maurice Kevenaar <maurice@kevenaar.name>
2020-09-06 21:13:11 +0200

    (GH-13) Approved and Unapproved package versions

--- Tests/Public/Get-LatestApprovedPackageVersion.Tests.ps1
+++ Tests/Public/Get-LatestApprovedPackageVersion.Tests.ps1
@@ -1,1 +1,47 @@
-# TODO
+#Requires -Modules chocolatey-diff
+
+Describe "Get-LatestApprovedPackageVersion" {
+
+    Context 'Normal Tests' {
+
+        BeforeAll {
+            Mock 'Get-FullPackageInfo' { Write-Output $PackageData } -ModuleName 'chocolatey-diff'
+        }
+
+        #region Discovery
+        $testCases = @(
+            @{
+                PackageName = 'MyPackage'
+                PackageData = @{
+                    Id       = 'MyPackage'
+                    Versions = @(
+                        @{
+                            Version = '0.1.0'
+                            Status  = 'approved'
+                            Listed  = $true
+                        }
+                        @{
+                            Version = '0.2.0'
+                            Status  = 'submitted'
+                            Listed  = $false
+                        }
+                    )
+                }
+            }
+        )
+        #endregion Discovery
+
+        It 'Fetch latest approved package info for <PackageName>' -TestCases $testCases {
+            Param($PackageName, $PackageData)
+            $pkgInfo = InModuleScope 'chocolatey-diff' -Parameters @{
+                PackageName = $PackageName
+            } {
+                Param ($PackageName)
+                Get-LatestApprovedPackageVersion -PackageId $PackageName
+                Should -Invoke Get-FullPackageInfo -Exactly 1 -ModuleName 'chocolatey-diff'
+            }
+            $pkgInfo | Should -Not -BeNullOrEmpty
+            $pkgInfo.Version.ToString() | Should -BeExactly '0.1.0'
+        }
+    }
+}
